a4f6354b2045394cb4ef09925d8a107ccdce97b4
dadamimi336 <51362228+dadamimi336@users.noreply.github.com>
2021-11-30 16:53:21 +0100

    FAIL
    Script foireux

---
+++ Assets/Projet/2D/HUD/Scripts/IndividualCase.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IndividualCase : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int ID;
+    public Sprite SP0, SP1, SP2, SP3;
+    public Text vie;
+    public float RID;
+    public GameObject Stocage;
+
+
+    void Start()
+    {
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        if (GameObject.Find("SelectionLisrt").GetComponent<GestionSelect>().NBRS >= ID)
+        {
+
+            try  RID = Stocage.GetComponent<SelectionPlayer>().selectedUnits[ID].GetComponent<AgentStates>().TypeUnit;
+           catch ;
+               
+                
+              
+
+
+
+
+                if (RID >= 0)
+                {
+                    if (RID == 0)
+                    {
+                        gameObject.GetComponent<Image>().sprite = SP0;
+                    }
+                    else if (RID == 1)
+                    {
+                        gameObject.GetComponent<Image>().sprite = SP1;
+
+                    }
+                    else if (RID == 2)
+                    {
+                        gameObject.GetComponent<Image>().sprite = SP2;
+                    }
+
+                    else if (RID == 3)
+                    {
+                        gameObject.GetComponent<Image>().sprite = SP3;
+
+                    }
+                    else
+                    {
+                        gameObject.GetComponent<Image>().sprite = SP0;
+                    }
+
+                }
+                else
+                {
+
+                    gameObject.GetComponent<Image>().sprite = SP0;
+                }
+
+           
+
+
+
+
+        }
+        else
+        {
+
+            gameObject.GetComponent<Image>().sprite = SP0;
+        }
+
+
+    }
+}
