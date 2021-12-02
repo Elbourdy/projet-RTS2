b9d9ed2083d95c13fe809c3dc1625bb348d056b0
dadamimi336 <51362228+dadamimi336@users.noreply.github.com>
2021-12-02 10:19:47 +0100

    HUD

---
+++ Assets/Projet/2D/HUD/Scripts/ButtonAS.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class ButtonAS : MonoBehaviour
+{
+
+    public float ID, Perso;
+    public Sprite SP, SP2, SP3, SP4, SP10;
+        
+
+    void Start()
+    {
+        
+    }
+
+    void buttonpress()
+    {
+
+        if (Perso == 0)
+        {
+
+        }
+        else if (Perso == 1)
+        {
+
+
+        }
+        else if (Perso == 2)
+        {
+
+        }
+
+        else if (Perso == 10)
+        {
+            if (ID == 1)
+            {
+
+            }
+            else if (ID == 2)
+            {
+
+            
+            }
+            else if (ID == 3)
+            {
+
+
+            }
+            else if (ID == 4)
+            {
+
+
+            }
+            else
+            {
+
+            }
+
+        }
+        else
+        {
+
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+            Perso = GameObject.Find("Canvas").GetComponent<Gestion_HUD>().Perso;
+
+      if (Perso == 0)
+        {
+
+        }
+      else if(Perso == 1)
+        {
+
+
+        }
+       else if(Perso == 2)
+        {
+
+        }
+          
+      else if (Perso == 10)
+        {
+            
+
+        }
+      else
+        {
+
+        }
+
+
+    }
+}
