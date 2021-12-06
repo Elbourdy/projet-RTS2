1d607cc4cbb079f75e79e59a25c74295ca1901f6
FatFluffyBee <corentinvignaud@gmail.com>
2021-12-06 21:18:16 +0100

    Cycle Jour-Nuit Basics

--- Assets/Projet/Scripts/Scripts_Corentin/SunCycleBehavior.cs
+++ Assets/Projet/Scripts/Scripts_Corentin/SunCycleBehavior.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class SunCycleBehavior : MonoBehaviour
 {
+    public enum statesDay { Day, Night };
+    public enum statesSeason { Winter, Spring, Summer, Autumn }
+
     public Light light;
     public Gradient colorLightDay, colorLightNight;
     public float timeOfDay, timeOfNight;
+    public int numberOfDayInASeason = 10;
 
-    private float totalTimeOfADay, timer;
+    private float totalTimeOfADay, timerDayCount;
+    private int dayCount;
+
+    [Header("OnReadOnly")]
+    [SerializeField] private float hourDaytimeCurrentlyIn = 0f;
+    [SerializeField] private float hourInCurrentDayPeriod = 0f;
+    [SerializeField] private int numberOfDaysPassed = 0;
+    [SerializeField] private statesDay periodOfDay = statesDay.Day;
+    [SerializeField] private statesSeason currentSeason = statesSeason.Autumn;
+
 
 
     // Start is called before the first frame update
@@ -20,20 +33,55 @@ public class SunCycleBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (timer/totalTimeOfADay > 1)
+        if (timerDayCount / totalTimeOfADay > 1)
         {
-            timer = 0;
+            timerDayCount = 0;
+            numberOfDaysPassed++;
+
+            if (numberOfDaysPassed > 0 && numberOfDaysPassed%numberOfDayInASeason == 0)
+            {
+                switch(currentSeason)
+                {
+                    case statesSeason.Winter:
+                        currentSeason = statesSeason.Spring;
+                        break;
+
+                    case statesSeason.Spring:
+                        currentSeason = statesSeason.Summer;
+                        break;
+
+                    case statesSeason.Summer:
+                        currentSeason = statesSeason.Autumn;
+                        break;
+
+                    case statesSeason.Autumn:
+                        currentSeason = statesSeason.Winter;
+                        break;
+                }
+            }
         }
 
-        timer += Time.deltaTime;
+        timerDayCount += Time.deltaTime;
 
-        if (timer < timeOfDay)
+        if (timerDayCount < timeOfDay)
         {
-            light.color = colorLightDay.Evaluate(timer / timeOfDay);
+            light.color = colorLightDay.Evaluate(timerDayCount / timeOfDay);
+
+            periodOfDay = statesDay.Day;
+            hourInCurrentDayPeriod = timerDayCount / timeOfDay;
         }
         else
         {
-            light.color = colorLightNight.Evaluate((timer - timeOfDay) / timeOfNight);
+            light.color = colorLightNight.Evaluate((timerDayCount - timeOfDay) / timeOfNight);
+
+            periodOfDay = statesDay.Night;
+            hourInCurrentDayPeriod = (timerDayCount - timeOfDay) / timeOfNight;
         }
+
+        hourDaytimeCurrentlyIn = timerDayCount / totalTimeOfADay;
     }
+
+    
+
+
 }
