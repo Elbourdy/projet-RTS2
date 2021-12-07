ef49882fba70b532952ab023b6070a86ee967975
GuilooDev <50377986+GuilooDev@users.noreply.github.com>
2021-12-07 12:08:22 +0100

    Déplacement Cam

---
+++ Assets/Projet/Scripts/Scripts_Guillaume/DéplacementCam.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DéplacementCam : MonoBehaviour
+{
+    private Vector2 dir;
+    private Vector3 mousePositionOff;
+
+    public float speed;
+    public float offset;
+
+
+
+    private void Update()
+    {
+        CheckMousePosition();
+
+        if (dir != Vector2.zero)
+        {
+            MoveCam();
+        }
+    }
+
+    private void MoveCam()
+    {
+        var cameraDir = Vector3.forward * dir.x + Vector3.right * dir.y;
+        transform.position += cameraDir * speed * Time.deltaTime;
+    }
+
+
+    private void CheckMousePosition()
+    {
+        dir = Vector2.zero;
+        if (isLeft())
+        {
+            dir += new Vector2(1, 0);
+        }
+        if (isRight())
+        {
+            dir += new Vector2(-1, 0);
+        }
+        if (isTop())
+        {
+            dir += new Vector2(0, 1);
+        }
+        if (isBottom())
+        {
+            dir += new Vector2(0, -1);
+        }
+
+        Debug.Log(dir);
+    }
+
+    private bool isLeft()
+    {
+        mousePositionOff = new Vector3(Input.mousePosition.x - offset, Input.mousePosition.y, 0);
+        if (mousePositionOff.x < 0)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool isRight()
+    {
+        mousePositionOff = new Vector3(Input.mousePosition.x + offset, Input.mousePosition.y, 0);
+        if (mousePositionOff.x > Screen.width)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool isTop()
+    {
+        mousePositionOff = new Vector3(Input.mousePosition.x, Input.mousePosition.y + offset, 0);
+        if (mousePositionOff.y > Screen.height)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private bool isBottom()
+    {
+        mousePositionOff = new Vector3(Input.mousePosition.x, Input.mousePosition.y - offset, 0);
+        if (mousePositionOff.y < 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
