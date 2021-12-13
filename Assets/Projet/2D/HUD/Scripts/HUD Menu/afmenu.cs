b1e27cb71b9de1779e29ba623bb351f9ef2ce9d1
dadamimi336 <51362228+dadamimi336@users.noreply.github.com>
2021-12-13 20:40:54 +0100

    HUD up et Script

---
+++ Assets/Projet/2D/HUD/Scripts/HUD Menu/afmenu.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class afmenu : MonoBehaviour
+{
+
+    public GameObject Menu;
+    public GameObject EcrOption;
+    public GameObject EcrCam;
+    public GameObject EcrSelMiss;
+    public GameObject EcrMult;
+    public GameObject EcrRe;
+    public GameObject EcreCrea;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Menu.SetActive(true);
+        EcrOption.SetActive(false);
+        EcrCam.SetActive(false);
+        EcrSelMiss.SetActive(false);
+        EcrMult.SetActive(false);
+        EcrRe.SetActive(false);
+        EcreCrea.SetActive(false);
+
+    }
+
+  
+
+    public void EcranOption()
+    {
+        Menu.SetActive(false);
+        EcrOption.SetActive(true);
+        EcrCam.SetActive(false);
+        EcrSelMiss.SetActive(false);
+        EcrMult.SetActive(false);
+        EcrRe.SetActive(false);
+        EcreCrea.SetActive(false);
+
+
+    }
+
+    public void EcranCampagne()
+    {
+
+        Menu.SetActive(false);
+        EcrOption.SetActive(false);
+        EcrCam.SetActive(true);
+        EcrSelMiss.SetActive(false);
+        EcrMult.SetActive(false);
+        EcrRe.SetActive(false);
+        EcreCrea.SetActive(false);
+
+
+    }
+
+    public void EcranSelectionMulti()
+    {
+        Menu.SetActive(false);
+        EcrOption.SetActive(false);
+        EcrCam.SetActive(false);
+        EcrSelMiss.SetActive(true);
+        EcrMult.SetActive(false);
+        EcrRe.SetActive(false);
+        EcreCrea.SetActive(false);
+
+
+    }
+
+    public void EcranMultijoueur()
+    {
+
+        Menu.SetActive(false);
+        EcrOption.SetActive(false);
+        EcrCam.SetActive(false);
+        EcrSelMiss.SetActive(false);
+        EcrMult.SetActive(true);
+        EcrRe.SetActive(false);
+        EcreCrea.SetActive(false);
+
+
+
+    }
+
+    public void EcranRecherche()
+    {
+        Menu.SetActive(false);
+        EcrOption.SetActive(false);
+        EcrCam.SetActive(false);
+        EcrSelMiss.SetActive(false);
+        EcrMult.SetActive(false);
+        EcrRe.SetActive(true);
+        EcreCrea.SetActive(false);
+
+
+    }
+
+
+    public void EcranCreation()
+    {
+
+        Menu.SetActive(false);
+        EcrOption.SetActive(false);
+        EcrCam.SetActive(false);
+        EcrSelMiss.SetActive(false);
+        EcrMult.SetActive(false);
+        EcrRe.SetActive(false);
+        EcreCrea.SetActive(true);
+
+    }
+
+
+
+}
