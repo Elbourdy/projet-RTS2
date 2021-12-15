9d9118dcaf53fdf99daf920c353e458a7e2ba7ee
FatFluffyBee <corentinvignaud@gmail.com>
2021-12-15 14:53:11 +0100

    Cycle final

--- Assets/Projet/Scripts/Scripts_Corentin/CycleDisplayMap.cs
+++ Assets/Projet/Scripts/Scripts_Corentin/CycleDisplayMap.cs
@@ -16,7 +16,7 @@ public class CycleDisplayMap : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         float nDaysSeasons = sCB.numberOfDayInASeason;
         float nDaysPasses = sCB.numberOfDaysPassed;
