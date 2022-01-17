40820ec94af31fe5b5db65add907fcba17e419cd
dadamimi336 <51362228+dadamimi336@users.noreply.github.com>
2022-01-17 14:06:47 +0100

    Color Life

--- Assets/Projet/Scripts/Scripts_Guillaume/Agent_Type.cs
+++ Assets/Projet/Scripts/Scripts_Guillaume/Agent_Type.cs
@@ -1,16 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Agent_Type : MonoBehaviour
 {
     public enum TypeAgent {Ally, Enemy };
     public TypeAgent Type;
-
+    public GameObject Bare;
+    public Material Vert, Rouge;
     public bool isConstruction = false;
-
+    
 
     private SelectionPlayer sp;
+
+    public void Start()
+    {
+        if(Type == TypeAgent.Ally)
+            {
+            Bare.GetComponent<Image>().color = new Color32(0, 255, 0, 255);
+        }
+        if (Type == TypeAgent.Enemy)
+        {
+            Bare.GetComponent<Image>().color = new Color32(255, 0, 0, 255);
+        }
+    }
 
     private void OnEnable()
     {
