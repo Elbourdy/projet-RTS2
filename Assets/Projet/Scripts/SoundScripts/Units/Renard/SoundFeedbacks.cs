7d44ee4e546e68925f895e2ba73c0a62e7350e54
GuilooDev <guillaumecharier@hotmail.fr>
2022-01-30 23:48:24 +0100

    Sound Feedback sur selection unité

--- Assets/Projet/Scripts/SoundScripts/Units/Renard/SoundFeedbacks.cs
+++ Assets/Projet/Scripts/SoundScripts/Units/Renard/SoundFeedbacks.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class SoundFeedbacks : MonoBehaviour
 {
+
+
     [FMODUnity.EventRef]
     public string onAttackSound;
 
+    [FMODUnity.EventRef]
+    public string onSelectedSound;
+
+
+    
+
     public void LaunchAttackSound ()
     {
+        if(onAttackSound != null)
         FMODUnity.RuntimeManager.PlayOneShot(onAttackSound, transform.position);
+    }
+
+
+    public void LaunchSelectedSound()
+    {
+        if (onSelectedSound != null)
+        FMODUnity.RuntimeManager.PlayOneShot(onSelectedSound, transform.position);
+    }
+
+    public string GetSoundNameSelection()
+    {
+        return onSelectedSound;
     }
 }
