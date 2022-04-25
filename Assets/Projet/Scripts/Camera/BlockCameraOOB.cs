765f1ffc13ff7fac83308d25a9036bee72115814
GuilooDev <50377986+GuilooDev@users.noreply.github.com>
2022-04-25 22:00:52 +0200

    Bloque camera si en dehors de la map

---
+++ Assets/Projet/Scripts/Camera/BlockCameraOOB.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockCameraOOB : MonoBehaviour
+{
+    [SerializeField] private GameObject[] OOBPoints = new GameObject[4];
+
+
+    private void Update()
+    {
+        if (transform.position.x > OOBPoints[2].transform.position.x)
+        {
+            var currentPos = transform.position;
+            transform.position = new Vector3(OOBPoints[2].transform.position.x, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x < OOBPoints[3].transform.position.x)
+        {
+            var currentPos = transform.position;
+            transform.position = new Vector3(OOBPoints[3].transform.position.x, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z > OOBPoints[0].transform.position.z)
+        {
+            var currentPos = transform.position;
+            transform.position = new Vector3(transform.position.x, transform.position.y, OOBPoints[0].transform.position.z);
+        }
+        if (transform.position.z < OOBPoints[1].transform.position.z)
+        {
+            var currentPos = transform.position;
+            transform.position = new Vector3(transform.position.x, transform.position.y, OOBPoints[1].transform.position.z);
+        }
+    }
+}
