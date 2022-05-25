9d13e37e01ef278cdb91c0eb0a1dc8bedad88e2b
FatFluffyBee <corentinvignaud@gmail.com>
2022-05-25 18:13:40 +0200

    Spawner nouveau modèle + feedback

--- Assets/Projet/Scripts/Batiments/SpawnerAnimation.cs
+++ Assets/Projet/Scripts/Batiments/SpawnerAnimation.cs
@@ -4,16 +4,44 @@ using UnityEngine;
 
 public class SpawnerAnimation : MonoBehaviour
 {
-    public Animator animator;
+    public Light lightSource;
+    public Renderer rD;
+    public float minLight, maxLight, durationTrans = 1;
+    public Material materialOne, materialOff;
+    private bool activated = false, deactivated = true;
+    private float count;
+
 
     FMOD.Studio.EventInstance spawnerPlay;
 
+
+    private void Update()
+    {
+        if(activated)
+        {
+            lightSource.intensity = Mathf.Lerp(minLight, maxLight, count / durationTrans);
+            rD.material.Lerp(materialOne, materialOff, count / durationTrans);
+            count += Time.deltaTime;
+        }
+
+        if(deactivated)
+        {
+            lightSource.intensity = Mathf.Lerp(maxLight, minLight, count / durationTrans);
+            rD.material.Lerp(materialOff, materialOne, count / durationTrans);
+            count += Time.deltaTime;
+        }
+
+        if (count > durationTrans)
+            deactivated = false;
+    }
+
     public void StartNight()
     {
-        animator.SetBool("Charging", true);
         spawnerPlay = FMODUnity.RuntimeManager.CreateInstance("event:/Building/Build_Spawner/Build_Spawn_Rise/Build_Spawn_Rise");
         spawnerPlay.set3DAttributes(FMODUnity.RuntimeUtils.To3DAttributes(gameObject));
         spawnerPlay.start();
+        activated = true;
+        count = 0;
     }
 
     public void StartSpawn()
@@ -23,6 +51,12 @@ public class SpawnerAnimation : MonoBehaviour
 
     public void EndNight()
     {
-        animator.SetBool("Charging", false);
+        count = 0;
+        if (activated)
+        {
+            activated = false;
+            deactivated = true;
+        }
+            
     }
 }
